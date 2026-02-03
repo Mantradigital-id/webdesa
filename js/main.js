26f448c55179d4ecb49cb64fb45ab6bf6fd2b5f2
Mantra Digital Indonesia <inbox.agusmunandar@gmail.com>
2026-02-04 02:08:11 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// main.js
+// Ganti APPS_SCRIPT_WEB_APP_URL dengan URL Apps Script Web App
+const apiUrl = "https://script.google.com/macros/s/AKfycbxOQ5FBhNDSLgTNnhiGO8U_5V_Ug79CWjvtsnNQVFOHWzOi725JIoV0-j5dpLk2QMENyA/exec";
+let rawData=[], filteredData=[];
+// script fetch, filter, charts dan tabel sama seperti sebelumnya
